2548bdad41058071d16a8c1e39d0563461059cbe
dz1to4ka <117407093+illiaBabak@users.noreply.github.com>
2024-04-23 14:45:37 +0200

    Added infinite scroll using observer

--- src/components/ImagesList/index.tsx
+++ src/components/ImagesList/index.tsx
@@ -1,13 +1,36 @@
 import { useInfinitePhotos } from 'src/api/photos';
 import { ImageCard } from '../ImageCard';
+import { useRef } from 'react';
+import { Loader } from '../Loader';
+
+const OBSERVER_OPTIONS = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 1,
+};
 
 export const ImagesList = (): JSX.Element => {
-  const { data } = useInfinitePhotos();
-  const images = data?.pages[0].images;
+  const observer = useRef<IntersectionObserver | null>(null);
+  const { data, isFetchingNextPage, fetchNextPage } = useInfinitePhotos();
+  const images = data?.pages.flatMap((el) => el.images) ?? [];
+
+  const handleIntersect = (el: HTMLElement | null) => {
+    observer.current = new IntersectionObserver((entries) => {
+      if (!entries[0].isIntersecting) return;
+
+      if (images?.length && !isFetchingNextPage) fetchNextPage();
+    }, OBSERVER_OPTIONS);
+
+    if (el) observer.current.observe(el);
+  };
 
   return (
-    <div className='images-list'>
-      {images?.map((image, index) => <ImageCard image={image} key={`image-card-${image.created_at}-${index}`} />)}
-    </div>
+    <>
+      <div className='images-list'>
+        {images?.map((image, index) => <ImageCard image={image} key={`image-card-${image.created_at}-${index}`} />)}
+      </div>
+
+      {isFetchingNextPage ? <Loader /> : <div ref={handleIntersect} />}
+    </>
   );
 };
