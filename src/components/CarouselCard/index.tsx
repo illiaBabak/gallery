b93a4034414922e61afba81f97af546ec3880985
dz1to4ka <117407093+illiaBabak@users.noreply.github.com>
2024-04-29 10:46:57 +0200

    Added prisma zoom

--- src/components/CarouselCard/index.tsx
+++ src/components/CarouselCard/index.tsx
@@ -1,9 +1,17 @@
+import PrismaZoom from 'react-prismazoom';
+
 type Props = {
   imageUrl: string;
 };
 
-export const CarouselCard = ({ imageUrl }: Props): JSX.Element => (
-  <div className='carousel-card'>
-    <img className='carousel-img' src={imageUrl} alt='carousel-image' />
-  </div>
-);
+export const CarouselCard = ({ imageUrl }: Props): JSX.Element => {
+  return (
+    <div className='carousel-card'>
+      <div className='img-wrapper'>
+        <PrismaZoom scrollVelocity={0.3} minZoom={1} maxZoom={2.5} doubleTouchMaxDelay={700}>
+          <img className='carousel-img' src={imageUrl} alt='carousel-image' />
+        </PrismaZoom>
+      </div>
+    </div>
+  );
+};
